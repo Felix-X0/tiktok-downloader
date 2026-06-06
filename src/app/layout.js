c722ffc161613b2abd5d538fdad1cefb386b20f4
felixxx_X0 <48359859+Felix-X0@users.noreply.github.com>
2026-06-07 00:43:40 +0700

    Remove RootLayout component from layout.js
    Removed RootLayout component and its export.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,4 @@
 import './globals.css';
-
-// Contoh di dalam src/app/layout.js
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta name="google-site-verification" content="Kk_QsKz7-SZhAA0wzc3gkDUW5BkfQYd7uU63FlqCRw8" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
-}
-
 export const metadata = {
   title: 'Free TikTok Video Downloader HD - No Watermark',
   description: 'Download TikTok videos online without watermark in high quality HD. Fast, free, and works globally on all devices.',
